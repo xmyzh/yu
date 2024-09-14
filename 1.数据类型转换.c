116ec02b348581d44ec7126afb13178787019841
xmyzh <36882042+xmyzh@users.noreply.github.com>
2024-09-14 17:32:53 +0800

    Update 1.数据类型转换.c
    移除空格

--- 1.数据类型转换.c
+++ 1.数据类型转换.c
@@ -7,17 +7,17 @@ int main(void)
 	float f = 5.3;
 	double d = 12.64;
 	double result;
-	
+
 	//同类型数据之间运算，并输出结果
 	printf("a+c=%d\n", a+c);
 	printf("a+c=%c\n", a+c);
-	printf("f+d=%f\n", f+d); 
+	printf("f+d=%f\n", f+d);
 	/*
 	a+c=102
 	a+c=f
 	f+d=17.940000
 	*/
-	
+
 	//不同类型数据进行运算， 并输出结果
 	printf("a+d=%f\n", a+d);
 	printf("c+f=%f\n", c+f );
@@ -25,7 +25,7 @@ int main(void)
 	a+d=17.640000
 	c+f=102.300003
 	*/
-	
+
 	//四个变量进行混合运算，并输出结果
 	result = a + c + (d + f);
 	printf("doule = %f\n", result);
